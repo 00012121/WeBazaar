bfbdf6cf7aa65f20e236a4e0e83e25235bc3b93a
00012121 <wiut00012121@gmail.com>
2024-03-16 12:35:00 +0500

    Validating produt class inputs

--- WeBazaar/Models/Product.cs
+++ WeBazaar/Models/Product.cs
@@ -8,12 +8,16 @@ namespace WeBazaar.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile picture")]
+        [Required(ErrorMessage = "Profile picture is required")]
         public string ProfilePictureURL { get; set; }
 
-        [Display(Name="Full name")]
+        [Display(Name="Product name")]
+        [Required(ErrorMessage = "Name of the product is required")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Product name must be between 5 and 100 chars")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
         //Relationship
