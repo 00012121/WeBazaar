f7fd247c8dcbafa8087729518eba917345bbacd4
00012121 <wiut00012121@gmail.com>
2024-03-16 11:09:33 +0500

    IProduct service updated

--- WeBazaar/Data/Services/IProductsService.cs
+++ WeBazaar/Data/Services/IProductsService.cs
@@ -1,6 +1,13 @@
-﻿namespace WeBazaar.Data.Services
+﻿using WeBazaar.Models;
+
+namespace WeBazaar.Data.Services
 {
     public interface IProductsService
     {
+        IEnumerable<Product> GetAll();
+        Product GetById(int id);
+        void Add(Product product);
+        Product Update(int id, Product newProduct);
+        void Delete(int id);
     }
 }
