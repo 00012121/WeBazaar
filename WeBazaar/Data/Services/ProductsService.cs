8e9000d7a4b887393518b343670dc4617d5072c7
00012121 <wiut00012121@gmail.com>
2024-03-16 11:13:23 +0500

    Added first service

---
+++ WeBazaar/Data/Services/ProductsService.cs
@@ -0,0 +1,32 @@
+﻿using WeBazaar.Models;
+
+namespace WeBazaar.Data.Services
+{
+    public class ProductsService : IProductsService
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Update(int id, Product newProduct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
