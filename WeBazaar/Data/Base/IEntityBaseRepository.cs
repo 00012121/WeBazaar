9e10ad8ea273ac1290326f75edc5b25c1d094337
00012121 <wiut00012121@gmail.com>
2024-03-18 14:32:33 +0500

    Entity repository

---
+++ WeBazaar/Data/Base/IEntityBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using WeBazaar.Models;
+
+namespace WeBazaar.Data.Base
+{
+    public interface IEntityBaseRepository<T> where T: class, IEntityBase
+    { 
+            Task<IEnumerable<T>> GetAllAsync();
+            Task<T> GetByIdAsync(int id);
+            Task AddAsync(T entity);
+            Task<T> UpdateAsync(int id, T entity);
+            Task DeleteAsync(int id);
+    }
+}
