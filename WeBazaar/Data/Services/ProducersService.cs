a8cbc84b67b43c25cc8ad2700615e23e997ff1bf
00012121 <wiut00012121@gmail.com>
2024-03-18 22:04:52 +0500

    Producer action services

---
+++ WeBazaar/Data/Services/ProducersService.cs
@@ -0,0 +1,13 @@
+﻿using WeBazaar.Data.Base;
+using WeBazaar.Models;
+
+namespace WeBazaar.Data.Services
+{
+    public class ProducersService:EntityBaseRepository<Producer>, IProducersService
+    {
+        public ProducersService(AppDbContext context) : base(context)
+        {
+                
+        }
+    }
+}
