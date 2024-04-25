22d4d53c2bfa4d1800177d731f0ac912388feae0
00012121 <wiut00012121@gmail.com>
2024-04-25 15:49:39 +0500

    Query

--- WeBazaar/Data/Base/IEntityBaseRepository.cs
+++ WeBazaar/Data/Base/IEntityBaseRepository.cs
@@ -1,10 +1,12 @@
-﻿using WeBazaar.Models;
+﻿using System.Linq.Expressions;
+using WeBazaar.Models;
 
 namespace WeBazaar.Data.Base
 {
     public interface IEntityBaseRepository<T> where T: class, IEntityBase
     { 
             Task<IEnumerable<T>> GetAllAsync();
+            Task<IEnumerable<T>> GetAsync(params Expression<Func<T, object>>[] includeProperties);
             Task<T> GetByIdAsync(int id);
             Task AddAsync(T entity);
             Task UpdateAsync(int id, T entity);
