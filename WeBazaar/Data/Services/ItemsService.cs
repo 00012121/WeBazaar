eee5fc1f6fb2de4c1bda79da0a1d4e0bfd8b56da
00012121 <wiut00012121@gmail.com>
2024-04-27 00:25:57 +0500

    Add new button for one file for all

--- WeBazaar/Data/Services/ItemsService.cs
+++ WeBazaar/Data/Services/ItemsService.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using WeBazaar.Data.Base;
+using WeBazaar.Data.ViewModels;
 using WeBazaar.Models;
 
 namespace WeBazaar.Data.Services
 {
-    public class ItemsService:EntityBaseRepository<Item>, IItemsService
+    public class ItemsService : EntityBaseRepository<Item>, IItemsService
     {
         private readonly AppDbContext _context;
-        public ItemsService(AppDbContext context):base (context) 
-        { 
+        public ItemsService(AppDbContext context) : base(context)
+        {
             _context = context;
         }
 
@@ -21,6 +22,18 @@ namespace WeBazaar.Data.Services
                 .FirstOrDefaultAsync(n => n.Id == id);
 
             return itemDetails;
+        }
+
+        public async Task<NewItemDropdownsVM> GetNewItemDropdownsValues()
+        {
+            var response = new NewItemDropdownsVM()
+            {
+                Products = await _context.Products.OrderBy(n => n.FullName).ToListAsync(),
+                Companies = await _context.Companies.OrderBy(n => n.Name).ToListAsync(),
+                Producers = await _context.Producers.OrderBy(n => n.FullName).ToListAsync()
+            };
+
+            return response;
         }
     }
 }
