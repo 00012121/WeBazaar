e4151bd49e17ea2d5f4f1ba6d83ae263b6f3cc34
00012121 <wiut00012121@gmail.com>
2024-04-27 12:01:02 +0500

    Searching an item

--- WeBazaar/Controllers/ItemsController.cs
+++ WeBazaar/Controllers/ItemsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
 using WeBazaar.Data;
 using WeBazaar.Data.Services;
 using WeBazaar.Models;
@@ -21,6 +22,19 @@ namespace WeBazaar.Controllers
         {
             var allItems = await _service.GetAllAsync(n => n.Company);
             return View(allItems);
+        }
+
+        public async Task<IActionResult> Filter(string searchString)
+        {
+            var allItems = await _service.GetAllAsync(n => n.Company);
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                var filteredResult = allItems.Where(n => n.Name.Contains(searchString) || n.Description.Contains(searchString)).ToList();
+                return View("Index", filteredResult);
+            }
+
+            return View("Index", allItems);
         }
 
         //Get: Items/Details/1
@@ -57,6 +71,57 @@ namespace WeBazaar.Controllers
             }
 
             await _service.AddNewItemAsync(item);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        //GET: Items/Edit 
+        public async Task<IActionResult> Edit(int id)
+        {
+            var itemDetails = await _service.GetItemByIdAsync(id);
+            if (itemDetails == null) return View("NotFound");
+
+            var response = new NewItemVM
+            {
+                Id = itemDetails.Id,
+                Name = itemDetails.Name,
+                Description = itemDetails.Description,
+                Price = itemDetails.Price,
+                ImageURL = itemDetails.ImageURL,
+                StartDate = itemDetails.StartDate,
+                EndDate = itemDetails.EndDate,
+                ItemCategory = itemDetails.ItemCategory,
+                CompanyId = itemDetails.CompanyId,
+                ProducerId = itemDetails.ProducerId,
+                ProductIds = itemDetails.Products_Items.Select(n => n.ProductId).ToList(),
+            };
+
+            var itemDropdownsData = await _service.GetNewItemDropdownsValues();
+
+            ViewBag.Companies = new SelectList(itemDropdownsData.Companies, "Id", "Name");
+            ViewBag.Products = new SelectList(itemDropdownsData.Products, "Id", "FullName");
+            ViewBag.Producers = new SelectList(itemDropdownsData.Producers, "Id", "FullName");
+
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, NewItemVM item)
+        {
+            if (id != item.Id) return View("NotFound");
+
+            if (!ModelState.IsValid)
+            {
+                var itemDropdownsData = await _service.GetNewItemDropdownsValues();
+
+                ViewBag.Companies = new SelectList(itemDropdownsData.Companies, "Id", "Name");
+                ViewBag.Products = new SelectList(itemDropdownsData.Products, "Id", "FullName");
+                ViewBag.Producers = new SelectList(itemDropdownsData.Producers, "Id", "FullName");
+
+                return View(item);
+            }
+
+            await _service.UpdateItemAsync(item);
             return RedirectToAction(nameof(Index));
         }
     }
