54ed1d416e632ebb2723ee56a9f847882473040e
00012121 <wiut00012121@gmail.com>
2024-04-27 17:19:26 +0500

    Adding OdersController file

---
+++ WeBazaar/Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeBazaar.Data.Cart;
+using WeBazaar.Data.Services;
+using WeBazaar.Data.ViewModels;
+
+namespace WeBazaar.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IItemsService _itemsService;
+        private readonly ShoppingCart _shoppingCart;
+        public OrdersController(IItemsService itemsService, ShoppingCart shoppingCart)
+        {
+            _itemsService = itemsService;    
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
---
+++ WeBazaar/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using WeBazaar.Data.Cart;
+
+namespace WeBazaar.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
