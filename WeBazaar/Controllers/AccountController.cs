48d1bd97d94d621eaf8f01e7ab3f9485f8397fae
00012121 <wiut00012121@gmail.com>
2024-04-28 20:43:11 +0500

    Signing up users

--- WeBazaar/Controllers/AccountController.cs
+++ WeBazaar/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Runtime.InteropServices;
 using WeBazaar.Data;
+using WeBazaar.Data.Static;
 using WeBazaar.Data.ViewModels;
 using WeBazaar.Models;
 
@@ -36,7 +37,7 @@ namespace WeBazaar.Controllers
                 if (passworkCheck)
                 {
                     var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
-                    if(result.Succeeded)
+                    if (result.Succeeded)
                     {
                         return RedirectToAction("Index", "Items");
                     }
@@ -48,5 +49,31 @@ namespace WeBazaar.Controllers
         }
 
         public IActionResult Register() => View(new RegisterVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null) {
+                TempData["Error"] = "This email address is already taken";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+            return View("RegisterCompleted");
+        }
     }
 }
